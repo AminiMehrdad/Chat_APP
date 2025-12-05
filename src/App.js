6a50143715632d99ae15f0aec3fd63e596ac7633
Mehrdad1010 <mehrdadamini1400@gmail.com>
2025-12-05 11:20:43 +0330

    chate the configlict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import { Link, Route, Routes } from "react-router";
 import Login from "./pages/loginPage";
 import Chat from "./pages/chatPage";
-import UserChat from "./pages/userChatPage";;
+import UserChat from "./pages/userChatPage";
 
 function App() {
   return (
-    <>
-     
-      {/* <nav>
+    <div>
+      <nav>
         <Link to="/login">Login</Link> | <Link to="/chat">Chat</Link> |{" "}
         <Link to="/userchat">User Chat</Link>
-      </nav> */}
+      </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/userchat" element={<UserChat />} />
         <Route path="/" element={<Login />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/pages/chatPage/Chat.jsx
+++ src/pages/chatPage/Chat.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import NewMessage from '../../components/NewMessage/NewMessage';
@@ -39,6 +40,18 @@ function Chat() {
           <Conversation contact={selectedContact} />
         </div>
       </div>
+=======
+import { Link } from "react-router";
+
+function Chat() {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <p>This is the main chat page.</p>
+
+      {/* example link to user chat */}
+      <Link to="/userchat">Go to User Chat</Link>
+>>>>>>> parent of 5ba0527 (add chat template)
     </div>
   );
 }
